2ff9bdd9e0e42829a058bef311150aa47ee1a61b
Rushikesh Badgujar <82447941+rishi7822@users.noreply.github.com>
2023-01-07 01:04:58 +0530

    Add files via upload

---
+++ dictupdate.py
@@ -0,0 +1,21 @@
+#The update() method will update the dictionary with the items from the given argument.
+
+#The argument must be a dictionary, or an iterable object with key:value pairs.
+
+
+
+thisdict = {
+  "brand": "Ferrari",
+  "model": "Enzo",
+  "year": 1980,
+}
+thisdict["year"] = 2023
+print(thidict)
+
+
+thisdict = {
+  "brand": "Ford",
+  "model": "Mustang",
+  "year": 1964
+}
+thisdict.update({"year": 2020})
